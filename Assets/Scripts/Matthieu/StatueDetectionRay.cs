c30acc29d73db96fbffca8a3725aa87c0837d7c1
Matthieu Delannoy <dinomanga2@gmail.com>
2021-02-11 16:36:39 +0100

    Raycast Yeux
    J'ai ajouté le raycast sur les yeux de 173

---
+++ Assets/Scripts/Matthieu/StatueDetectionRay.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class StatueDetectionRay : MonoBehaviour
+{
+    public Vector3 direction;
+    public float distance;
+    public TimeGauge gauge;
+    public GameObject eyes;
+    public GameObject player;
+
+    [SerializeField] private Color gizmoColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction =  player.transform.position- eyes.transform.position;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(eyes.transform.position,direction,out hit,distance))
+        {
+
+            Debug.Log(hit.collider);
+            if (hit.collider!=null)
+            {
+                if (hit.collider.tag == ("Statue"))
+                {
+                    Debug.Log("c touché");
+                }
+            }
+
+
+
+
+        }
+        else
+        {
+            gauge.AugmentTime();
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        direction = player.transform.position - eyes.transform.position;
+
+        Gizmos.DrawRay(eyes.transform.position, direction );
+    }
+}
---
+++ Assets/Scripts/Matthieu/TimeGauge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeGauge : MonoBehaviour
+{
+    public float timeMultiplier = 0.2f;
+
+    public float startValue = 0;
+
+   [SerializeField] private Slider slider;
+
+    private void Start()
+    {
+        slider.value = startValue;
+    }
+
+
+    public void AugmentTime()
+    {
+        slider.value += Time.deltaTime * timeMultiplier;
+    }
+}
