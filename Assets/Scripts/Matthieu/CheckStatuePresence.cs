f40df2da47c8905f209d008aff6d47ddee65727a
Matthieu Delannoy <dinomanga2@gmail.com>
2021-03-04 10:38:02 +0100

    Ajout Détection des positions
    Création d'un script qui détecte si la statue est sur la bonne position ou non

---
+++ Assets/Scripts/Matthieu/CheckStatuePresence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckStatuePresence : MonoBehaviour
+{
+
+    public bool statueIsHere = false;
+   
+
+    private void Update()
+    {
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 0.3f,Vector3.forward);
+
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.gameObject.tag == "Statue")
+            {
+               
+                statueIsHere = true;
+            }
+            else
+            {
+                statueIsHere = false;
+            }
+        }
+    }
+
+
+}
