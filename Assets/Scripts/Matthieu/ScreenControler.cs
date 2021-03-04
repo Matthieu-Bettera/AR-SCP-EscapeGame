45c7090bf3284961cbc5512b96bb3b1587f98b9c
Matthieu Delannoy <dinomanga2@gmail.com>
2021-03-04 11:05:24 +0100

    Ecran
    Les écrans s'éteigne et se rallume en fonction de la position de la statue
    Soucis à corriger : La statue est à une position mais ne s'affiche pas

---
+++ Assets/Scripts/Matthieu/ScreenControler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenControler : MonoBehaviour
+{
+
+    public Material screenOff;
+    public Material screenOn;
+    MeshRenderer screenRenderer;
+
+    [SerializeField]Transform linkedPosition;
+
+    public CheckStatuePresence presence;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenRenderer = this.gameObject.GetComponent<MeshRenderer>();
+
+        screenRenderer.material = screenOff;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (presence.statueIsHere == true)
+        {
+            screenRenderer.material = screenOn;
+        }
+        else
+        {
+            screenRenderer.material = screenOff;
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(transform.position, linkedPosition.position);
+    }
+}
