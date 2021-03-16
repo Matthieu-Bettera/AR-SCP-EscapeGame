977cd5b508776a42bbd90e77df9dd13e1d9c3e15
Raven <p.tahon24@gmail.com>
2021-03-16 21:30:14 +0100

    Menu_Principale et sous menu de code à  chiffre
    => ajout du menu_principale , du sous_menu machine et du sous_menu code
    => à venir : Amélioration du texte de l'enigme Malo , Ajout d'illustration , Ajout du sous_menu Rapport

---
+++ Assets/Main_Menu/Scripts/Check_Code.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Check_Code : MonoBehaviour
+{
+
+    string Password_Malo;
+    string Password_682;
+    string LastPassword;
+    string Result;
+    public List<Text> MachineText = new List<Text>();
+    public List<Text> CodeText = new List<Text>();   
+    [SerializeField] string SceneName; 
+
+    private void Start()
+    {
+        LastPassword = "333433";
+        Password_682 = "682";
+        Password_Malo ="471"; 
+    }
+    public void ConfirmMachine()
+    {
+        Result = MachineText[0].text + MachineText[1].text + MachineText[2].text;
+        if(Result == Password_682)
+        {
+            Debug.Log("SCENE_682");
+        }
+        if(Result == Password_Malo)
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+    public void ConfirmCode()
+    {
+        Result = CodeText[0].text + CodeText[1].text + CodeText[2].text+ CodeText[3].text+ CodeText[4].text+ CodeText[5].text;
+        if(Result == LastPassword)
+        {
+            Debug.Log("SCENE_FINAL");
+        }
+    }
+}
---
+++ Assets/Main_Menu/Scripts/SwitchToMachine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchToMachine : MonoBehaviour
+{
+    [SerializeField]private Animator BlackScreen_Machine;
+    [SerializeField] private Animator BlackScreen_Code;
+    // Start is called before the first frame update
+ 
+    public void ShowMachine()
+    {
+        BlackScreen_Machine.SetBool("Fade", true);
+        BlackScreen_Machine.SetBool("FadeOut", false);
+    }
+    public void ShowCode()
+    {
+        BlackScreen_Code.SetBool("Fade", true);
+        BlackScreen_Code.SetBool("FadeOut", false);
+    }
+    public void CodeToMenu()
+    {
+              
+        BlackScreen_Code.SetBool("FadeOut", true);
+        BlackScreen_Code.SetBool("Fade", false);
+    }
+    public void MachineToMenu()
+    {
+       
+        BlackScreen_Machine.SetBool("FadeOut", true);
+        BlackScreen_Machine.SetBool("Fade", false);
+    }
+}
