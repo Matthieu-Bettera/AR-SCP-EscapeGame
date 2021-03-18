334299eb3b67a2fcbb45c6b7f07455eebda0631d
Raven <p.tahon24@gmail.com>
2021-03-18 13:24:06 +0100

    Update_Malo
    pppp

--- Assets/Main_Menu/Scripts/Check_Code.cs
+++ Assets/Main_Menu/Scripts/Check_Code.cs
@@ -12,18 +12,20 @@ public class Check_Code : MonoBehaviour
     string LastPassword;
     string Result;
     public List<Text> MachineText = new List<Text>();
-    public List<Text> CodeText = new List<Text>();   
+    public List<Text> CodeText = new List<Text>();
+    public List<Text> RapportText = new List<Text>();
+    public List<Rapports> Rapport_SCP = new List<Rapports>();
     [SerializeField] string SceneName; 
 
     private void Start()
     {
-        LastPassword = "333433";
-        Password_682 = "682";
-        Password_Malo ="471"; 
+        LastPassword = "461829";
+        Password_682 = "83";
+        Password_Malo ="61"; 
     }
     public void ConfirmMachine()
     {
-        Result = MachineText[0].text + MachineText[1].text + MachineText[2].text;
+        Result = MachineText[0].text + MachineText[1].text;
         if(Result == Password_682)
         {
             Debug.Log("SCENE_682");
@@ -39,6 +41,17 @@ public class Check_Code : MonoBehaviour
         if(Result == LastPassword)
         {
             Debug.Log("SCENE_FINAL");
+        }
+    }
+    public void ConfirmRapport()
+    {
+     Result = RapportText[0].text + RapportText[1].text + RapportText[2].text;
+        foreach (Rapports _rapport in Rapport_SCP) 
+        { 
+            if(Result == _rapport.Code)
+            {
+                _rapport.Rapport_Enable();
+            }
         }
     }
 }
